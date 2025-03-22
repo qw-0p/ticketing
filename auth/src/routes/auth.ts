5516e12a759e8bcb02348c5b0c4a19f322c405e9
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-03-22 20:57:30 +0200

    added validation

--- auth/src/routes/auth.ts
+++ auth/src/routes/auth.ts
@@ -1,13 +1,23 @@
-import express from "express";
+import express, {Request, Response} from "express";
+import {body} from 'express-validator';
 
 const authRouter = express.Router();
+
 
 authRouter.get('/currentuser', (req, res) => {
     res.send('Hi there');
 })
 
-authRouter.post('/signup', (req, res) => {
-    res.send('Hi there');
+authRouter.post('/signup', [
+   body('email').isEmail().withMessage('Email must be valid'),
+   body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
+],(req: Request, res: Response) => {
+    const {email, password} = req.body;
+
+    if (!email || typeof email !== 'string') {
+        res.status(400).send('email is required');
+    }
+
 })
 
 authRouter.post('/signin', (req, res) => {
