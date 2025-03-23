247bf67589483af18c902ea51e3a6b280d7e4afe
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-03-23 19:23:28 +0200

    handling validation errors

--- auth/src/routes/auth.ts
+++ auth/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import {body} from 'express-validator';
+import {body, validationResult} from 'express-validator';
 
 const authRouter = express.Router();
 
@@ -11,12 +11,16 @@ authRouter.get('/currentuser', (req, res) => {
 authRouter.post('/signup', [
    body('email').isEmail().withMessage('Email must be valid'),
    body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
-],(req: Request, res: Response) => {
+],(req: Request, res: Response): any => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+
     const {email, password} = req.body;
 
-    if (!email || typeof email !== 'string') {
-        res.status(400).send('email is required');
-    }
+    res.send({});
 
 })
 
